b56cabf6288c458941562f4ba92a65935e4f370f
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 18:38:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from pyrogram import *
 import vars
 import os
 import sys
+from time import time, strftime, gmtime
 import time
 import asyncio
 from tu import humanbytes, progress_for_pyrogram, check_is_streamable, get_video_duration,  take_ss, TimeFormatter, get_width_height
