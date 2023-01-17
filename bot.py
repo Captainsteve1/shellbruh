60579caeb787c957bc767e6442aeab774dcf2f4f
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 15:58:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from pyrogram import *
 import vars
-
+import os
+import sys
+from os import path as os_path
 
 tony = Client(name="My Tony", api_id=vars.api_id, api_hash=vars.api_hash, bot_token=vars.bot_token)
 
@@ -15,6 +17,32 @@ def help(client, message):
        message.reply_text(text="Helps")
     else:
        message.reply_text(text="No helps")
+
+@tony.on_message(filters.command('shell') & filters.private)
+def shell(client, message):
+    message = update.effective_message
+    cmd = message.text
+    process = srun(cmd, capture_output=True, shell=True)
+    reply = ''
+    stdout = process.stdout.decode('utf-8')
+    stderr = process.stderr.decode('utf-8')
+    if len(stdout) != 0:
+        reply += f"<b>Stdout</b>\n<code>{stdout}</code>\n"
+    if len(stderr) != 0:
+        reply += f"<b>Stderr</b>\n<code>{stderr}</code>\n"
+    if len(reply) > 3000:
+        with open('output.txt', 'w') as file:
+            file.write(reply)
+        with open('output.txt', 'rb') as doc:
+            context.bot.send_document(
+                document=doc,
+                filename=doc.name,
+                reply_to_message_id=message.message_id,
+                chat_id=message.chat_id)
+    elif len(reply) != 0:
+        sendMessage(reply, context.bot, update.message)
+    else:
+        sendMessage('Executed', context.bot, update.message)
 
 print("starting bot!..")
 print("Bot started../")
