54d63a2de0c6eb513dbbaef82369875760d54ed3
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 15:13:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import vars
 tony = Client (api_id=vars.id, api_hash=vars.hash, bot_token=vars.token)
 
 @tony.on_message()
-def hi(client, message)
+def hi(client, message):
     print(message)
 
 print("Starting up bot../")
