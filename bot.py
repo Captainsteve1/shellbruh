f32e9170ed308ebef2a8acf75365fbc47ced2469
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 17:46:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -90,12 +90,7 @@ async def tg_up(input_str, message, sts_msg, drm=True):
     else:
         thumb = None
     file_name = os_path.basename(input_str)
-  
-#    if check_is_streamable(file_name):
-        try:
-            duration = await get_video_duration(input_str)
-        except:
-            duration = None
+
         if thumb is None:
             thumb = await take_ss(input_str)
         sent_msg = await client.send_video(chat_id=chat_id,
