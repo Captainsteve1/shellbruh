9699fad72ea92f508fc3f7d70977c4709fc55a4d
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 15:18:42 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
-id = 15855531
-hash = "31e0b87de4285ebff259e003f58bf469"
-token = "2083076297:AAGLVpm0CTNgOQwYkVLsNoMBbSz4Unz35uY"
+api_id = 15855531
+api_hash = "31e0b87de4285ebff259e003f58bf469"
+bot_token = "2083076297:AAGLVpm0CTNgOQwYkVLsNoMBbSz4Unz35uY"
