12eb8c23857ddfdaac7bdfab4b139c40591e0d3d
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 17:26:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,11 +95,10 @@ def tg_up(input_str, message, sts_msg, drm=True):
             duration = await get_video_duration(input_str)
         except:
             duration = None
-    if thumb is None:
-            thumb = None
-take_ss(input_str)
+        if thumb is None:
+            thumb = await take_ss(input_str)
         sent_msg = await client.send_video(chat_id=chat_id,
-                                video=input_str,
+                                video=input_str
                                   thumb=thumb
                                   progress=progress_for_pyrogram,
                                   progress_args=("Uploading",
