7d04006f9254d1d6052cebadff1d88409e93c49f
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 17:24:54 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,8 +96,8 @@ def tg_up(input_str, message, sts_msg, drm=True):
         except:
             duration = None
     if thumb is None:
-            thumb = 
-await take_ss(input_str)
+            thumb = None
+take_ss(input_str)
         sent_msg = await client.send_video(chat_id=chat_id,
                                 video=input_str,
                                   thumb=thumb
