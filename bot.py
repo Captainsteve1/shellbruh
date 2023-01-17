d03cab8452fb2daa416c4c1992dac137b77d9734
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 19:02:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -116,3 +116,9 @@ print("well starting up")
 print("Bot started..")
 
 asyncio.run()
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(startBot()) 
+    asyncio.get_event_loop().run_forever() 
+    asyncio.get_event_loop().close() 
+    sys.exit(0)
