a18e02d5c29e3ff50684e4fc2259668b2c0cf82a
Captainsteve1 <108216191+Captainsteve1@users.noreply.github.com>
2023-01-17 15:35:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ tony = Client(name="My Tony", api_id=vars.api_id, api_hash=vars.api_hash, bot_to
 
 @tony.on_message(filters.command('start') & filters.private)
 def start(client, message):
+    message.reply_text(text="welcome to ANToNi rip bot")
     print("Welcome to tony bot")
 
 
